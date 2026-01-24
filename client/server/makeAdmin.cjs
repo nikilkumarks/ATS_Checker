80683dca5cad6967db75cbe589606dec3008df20
NIKIL KUMAR K S <nikilkumark.s19601@gmail.com>
2026-01-24 11:05:45 +0530

    Add makeAdmin script to promote user to admin

---
+++ client/server/makeAdmin.cjs
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const User = require("./models/users.cjs");
+require("dotenv").config();
+
+mongoose.connect(process.env.MONGO_URI).then(async () => {
+    // Replace with your registered email
+    await User.findOneAndUpdate({ email: "nikil123@test.com" }, { role: "admin" });
+    console.log("Success: You are now an Admin!");
+    process.exit();
+});                      
